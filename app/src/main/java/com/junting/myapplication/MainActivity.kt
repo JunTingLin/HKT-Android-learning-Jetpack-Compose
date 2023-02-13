2e80dc562497b071782388f9f44b66ecee570c69
junting <10355098@st.thsh.tp.edu.tw>
2023-02-13 20:12:09 +0800

    ConstraintLayout

--- app/src/main/java/com/junting/myapplication/MainActivity.kt
+++ app/src/main/java/com/junting/myapplication/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import com.junting.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,34 +25,44 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MixDemo() {
-    Row(
+    ConstraintLayout(
         modifier = Modifier
+            .fillMaxHeight()
             .fillMaxWidth()
-            .padding(10.dp),
-        verticalAlignment = Alignment.CenterVertically
     ) {
-        Box(
-            modifier = Modifier
-                .size(100.dp)
-                .background(Color.Yellow)
-        ) {
+        val (yellowBox, greenBox, redBox) = createRefs()
 
-        }
-        Column(modifier = Modifier.padding(start = 20.dp)) {
-            Box(
-                Modifier
-                    .width(250.dp)
-                    .height(30.dp)
-                    .background(Color.Green)
-            )
-            Box(
-                Modifier
-                    .padding(top = 10.dp)
-                    .width(150.dp)
-                    .height(30.dp)
-                    .background(Color.Red)
-            )
-        }
+        //對齊父容器
+        Box(modifier = Modifier
+            .size(100.dp)
+            .background(Color.Yellow)
+            .constrainAs(yellowBox) {
+                top.linkTo(parent.top)
+                bottom.linkTo(parent.bottom)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+            }
+        )
+
+        //對齊父容器，加入 margin 間距
+        Box(modifier = Modifier
+            .size(50.dp)
+            .background(Color.Green)
+            .constrainAs(greenBox) {
+                top.linkTo(parent.top,150.dp)
+                bottom.linkTo(parent.bottom)
+                start.linkTo(parent.start,150.dp)
+                end.linkTo(parent.end)
+            })
+
+        //對齊其他元件，並加入 margin 間距
+        Box(modifier = Modifier
+            .size(50.dp)
+            .background(Color.Red)
+            .constrainAs(redBox) {
+                top.linkTo(greenBox.bottom)
+                end.linkTo(greenBox.end)
+            })
     }
 }
 
