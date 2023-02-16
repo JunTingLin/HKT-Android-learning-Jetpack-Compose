b6bbeb5b82e1e8152e52d24e7f5829da7a817851
junting <10355098@st.thsh.tp.edu.tw>
2023-02-16 19:22:43 +0800

    HW - hasbugs

--- app/src/main/java/com/junting/myapplication/MainActivity.kt
+++ app/src/main/java/com/junting/myapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.junting.myapplication
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -7,84 +8,136 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.constraintlayout.compose.ConstraintLayout
-import coil.compose.AsyncImage
-import com.junting.myapplication.ui.theme.MyApplicationTheme
+import com.google.gson.Gson
+import com.junting.myapplication.data.Feature
+import com.junting.myapplication.data.PharmacyInfo
+import com.junting.myapplication.util.OkHttpUtil
+import com.junting.myapplication.util.OkHttpUtil.Companion.mOkHttpUtil
+import okhttp3.*
+import java.io.IOException
 import java.util.*
 
+
+private var pharmacyList: List<Feature>? = null
+
 class MainActivity : ComponentActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContent {
-            DiceDemo()
+            Demo()
+        }
+        getPharmacyData()
+        Log.d("junting", "取完資料")
+
+    }
+    @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+    @Composable
+    fun Demo() {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { Text("junting練習") },
+                )
+            }
+        ) {
+            //畫面內容
+            pharmacyList?.let { it1 ->
+                PharmacyListScreen(it1)
+            }
+
+        }
+
+    }
+
+    @Composable
+    fun PharmacyListScreen(pharmacyList: List<Feature>) {
+        LazyColumn {
+            items(pharmacyList) { feature ->
+                PharmacyCard(feature)
+            }
         }
     }
-}
 
-@Composable
-fun DiceDemo() {
-    val diceImgs = arrayOf(
-        R.drawable.dice1,
-        R.drawable.dice2,
-        R.drawable.dice3,
-        R.drawable.dice4,
-        R.drawable.dice5,
-        R.drawable.dice6
-    )
-    var randNum by remember { mutableStateOf(0) }
+    @Composable
+    fun PharmacyCard(feature: Feature) {
+        Row(
+            Modifier
+                .padding(all = 20.dp)
+                .background(MaterialTheme.colors.background)
+                .fillMaxHeight(),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.dice1),
+                contentDescription = "",
 
-    ConstraintLayout(
-        modifier = Modifier
-            .fillMaxSize()
-    ) {
-        val (imgDice, btnRandom) = createRefs()
+                )
 
-        Image(painter = painterResource(id = diceImgs[randNum]),
-            contentDescription = "",
+            Column(
+                Modifier
+                    .padding(start = 20.dp)
+            ) {
+                Text(
+                    text = feature.properties.name,
+                    style = MaterialTheme.typography.subtitle1
+                )
+                Spacer(
+                    Modifier.height(4.dp)
+                )
+                Text(
+                    text = feature.properties.phone,
+                    style = MaterialTheme.typography.subtitle2
+                )
+            }
+        }
+    }
+
+
+    @Composable
+    fun SimpleCircularProgressComponent(isVisible: Boolean) {
+        val commentsAlpha = if (isVisible) 1f else 0f
+        CircularProgressIndicator(
             modifier = Modifier
-                .constrainAs(imgDice) {
-                    top.linkTo(parent.top, 50.dp)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-
-                }
-                .size(300.dp)
+                .padding(16.dp)
+                .alpha(commentsAlpha)
         )
+    }
 
-        Button(onClick = {
-            randNum = Random().nextInt(6)
-            Log.d("Junting", randNum.toString())
-        }, modifier = Modifier.constrainAs(btnRandom) {
-            bottom.linkTo(parent.bottom, 50.dp)
-            start.linkTo(parent.start)
-            end.linkTo(parent.end)
-        }) {
-            Text(text = "隨機", fontSize = 80.sp)
+    private fun getPharmacyData() {
+        //顯示忙碌圈圈
+//    SimpleCircularProgressComponent(isVisible = true)
 
-        }
+        mOkHttpUtil.getAsync(PHARMACIES_DATA_URL, object : OkHttpUtil.ICallback {
+            override fun onResponse(response: Response) {
+                val pharmaciesData = response.body?.string()
+
+                var pharmacyInfo: PharmacyInfo =
+                    Gson().fromJson(pharmaciesData, PharmacyInfo::class.java)
+                pharmacyList = pharmacyInfo.features
+                Log.d("junting", pharmaciesData ?: "空")
+
+            }
+
+            override fun onFailure(e: IOException) {
+                Log.d("junting", "onFailure: $e")
+
+            }
+        })
+
     }
 
 }
 
-fun calBMI(height: Double, weight: Double): Double {
-    return weight / Math.pow(height / 100, 2.0)
-}
-
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MyApplicationTheme(true) {
-        DiceDemo()
-    }
-}
